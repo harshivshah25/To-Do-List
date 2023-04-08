8d4e3fa2637f3f21c49b09695c44bf128db37751
Harshiv <b22ai047@iitj.ac.in>
2023-04-08 21:03:26 +0530

    To-do List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function TodoList() {
+  const [inputValue, setInputValue] = useState("");
+  const [timeValue, setTimeValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleTimeChange = (event) => {
+    setTimeValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue !== "" && timeValue !== "") {
+      setTodos([...todos, { task: inputValue, time: timeValue, completed: false }]);
+      setInputValue("");
+      setTimeValue("");
+    }
+  };
+
+  const handleCompleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = true;
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="To-do" style={{ color: "#3be23b" }}>
+        To-Do List
+      </h1>
+      <form>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Add a new task"
+        />
+        <input
+          type="text"
+          value={timeValue}
+          onChange={handleTimeChange}
+          placeholder="Add time for task"
+        />
+        <button className="button1" type="button" onClick={handleAddTodo}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? "completed" : ""}>
+            <span>
+              {todo.task} - {todo.time}
+            </span>
+            {!todo.completed && (
+              <button
+                className="button2"
+                type="button"
+                onClick={() => handleCompleteTodo(index)}
+                style={{ color: "#fff", backgroundColor: "Red" }}
+              >
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default TodoList;
